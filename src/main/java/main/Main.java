0169be2306de4c417915665606a8df28da7c7d62
Hamsterpants <kevinhoejgaard@gmail.com>
2021-10-25 10:09:42 +0200

    Reworked Main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,21 +1,11 @@
 package main;
 
-import account.Account;
-import account.AccountManager;
+import game.CarManager;
+import game.Game;
 
-import field.FieldManager;
-import field.InterfaceField;
-import field.Field;
+import java.awt.*;
 
-import player.InterfacePlayer;
-import player.Player;
-import player.PlayerManager;
-
-import die.DieManager;
-import die.Die;
-
-import game.CheckWin;
-
+import gui_fields.GUI_Car;
 public class Main {
 
     public static final long START_BALANCE = 1000;
@@ -25,77 +15,32 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //field
-        InterfaceField field2 = new Field(
-                2, "Tower", "You climb the tower for its riches and you get them", 250);
-        InterfaceField field3 = new Field(
-                3, "Crator", "You come across a crater and fall into it, ouch", -100);
-        InterfaceField field4 = new Field(
-                4, "Palace gates",
-                "You arrive at the foot of the giant Palace gates" +
-                        " and are greeted by the Palace maids," +
-                        " who hand you gifts",
-                100);
+        Game game = new Game(2);
 
-        InterfaceField field7 = new Field(
-                7, "Monastery",
-                "You come across a monastery, " +
-                        "the monks inside feed you and let you sleep in their quarters, " +
-                        "you feel well rested and get an extra turn",
-                0);
+        CarManager.createCar(
+                Color.GREEN,
+                Color.YELLOW,
+                GUI_Car.Type.TRACTOR,
+                GUI_Car.Pattern.HORIZONTAL_LINE
+        );
 
-        FieldManager fieldManager = new FieldManager(0, "Field manager");
+        CarManager.createCar(
+                Color.MAGENTA,
+                Color.CYAN,
+                GUI_Car.Type.UFO,
+                GUI_Car.Pattern.HORIZONTAL_LINE
+        );
 
-        //player
-        InterfacePlayer player1 = new Player(1, "Karl");
-        InterfacePlayer player2 = new Player(2, "Kevse");
+        //Create players and add them to PlayerManager
+        game.createPlayers(
+                "Slange",
+                "Kevse",
+                CarManager.getCar(1),
+                CarManager.getCar(2)
+        );
 
-        PlayerManager playerManager = new PlayerManager(0, "Player manager");
-
-        playerManager.addPlayer(player1);
-        playerManager.addPlayer(player2);
-
-        //account (could perhaps be shortened dynamically https://stackoverflow.com/questions/13868986/)
-        Account account1 = new Account(1, "Player 1 account", START_BALANCE);
-        Account account2 = new Account(2, "Player 2 account", START_BALANCE);
-
-        AccountManager accountManager = new AccountManager(0, "Account Manager");
-
-        accountManager.addAccount(account1);
-        accountManager.addAccount(account2);
-
-        fieldManager.addField(field2);
-        fieldManager.addField(field3);
-        fieldManager.addField(field4);
-        fieldManager.addField(field7);
-
-
-        fieldManager.getFieldDetails();
-
-
-        System.out.println(player1);
-
-        System.out.println(accountManager.getAccount(1).getBalance());
-        accountManager.getAccount(1).setBalance(-100);
-        System.out.println(accountManager.getAccount(1).getBalance());
-
-        System.out.println(accountManager.getAccount(2).getBalance());
-        accountManager.getAccount(2).setBalance(1999);
-        System.out.println(accountManager.getAccount(2).getBalance());
-        accountManager.getAccount(2).setBalance(0);
-        System.out.println(accountManager.getAccount(2).getBalance());
-        System.out.println(CheckWin.winCheck(2, accountManager, playerManager));
-
-        Die die1 = new Die(1, 1);
-        Die die2 = new Die(1,2);
-
-        DieManager dieManager = new DieManager(0, "Die Manager");
-        dieManager.addDie(die1);
-        dieManager.addDie(die2);
-
-        dieManager.throwDie();
-        System.out.println(dieManager.getDie(1));
-        System.out.println(dieManager.getDie(2));
+        game.playGame();
+        System.exit(0);
 
     }
 }
