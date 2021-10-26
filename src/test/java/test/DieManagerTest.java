8d0ccf45a09ab462aeb414a65f7372613fe606a2
SebastianHalfdanLauridsen <s215769@dtu.dk>
2021-10-27 01:49:15 +0200

    Reworked DieManagerTest into methods

--- src/test/java/test/DieManagerTest.java
+++ src/test/java/test/DieManagerTest.java
@@ -8,88 +8,143 @@ import game.Game;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import static org.junit.Assert.*;
 
 public class DieManagerTest {
 
     /**
-     * Tests if the throwDie method satisfies
-     *   the parameters of the minimum and maximum die value allowed
+     * Tests if the throwDie method satisfies the parameters of the minimum and maximum die value allowed
      */
     @Test
     public void throwDie() {
-        int die_amount = 2;
-        //create dice
-        //and adds them to DieManager's ArrayList
-        for (int i = 1; i <= die_amount; i++){
+        //print operative constants
+        System.out.println("Number of possible combinations with " + DIE_AMOUNT + " dies with " + DIE_SIDES + " sides: " + COMBINATION_TOTAL);
+        System.out.println("Number of possible sums with " + DIE_AMOUNT + " dies with " + DIE_SIDES + " sides: " + POSSIBLE_SUMS);
+
+        //create dice and adds them to DieManager's ArrayList
+        for (int i = 1; i <= DIE_AMOUNT; i++){
             Die die = new Die(1, i);
             DieManager.addDie(die);
         }
+        //run tests
+        diceTest();
+        theoryTest();
+    }
 
+    /**
+     * Throws the amount of dies and asserts that they are within the given values
+     */
+    public void diceTest(){
         //roll the dice
-
         DieManager.throwDies();
 
-        //determine if the face value of the first die is between the minimum allowed and the maximum allowed
-        for (int i = 1; i <= die_amount; i++){
+        //determine if the face value of the dice is between the minimum allowed and the maximum allowed
+        for (int i = 1; i <= DIE_AMOUNT; i++){
             int faceValue = DieManager.getDie(i).getFaceValue();
             assertTrue("Die is out of range, die: " + faceValue,
                     Game.DIE_MIN_VALUE <= faceValue && faceValue <= Game.DIE_MAX_VALUE);
         }
+    }
 
-        //sum test------------------------------------------------------------------
-        ArrayList<Integer> sumAmount =  new ArrayList<>();
-        for(int i = 0; i < 11; i++){
-            sumAmount.add(i, 0);
-        }
+    /**
+     * Asserts that the difference in the amount of sums
+     * compared to the theoretical possible are within the accepted error
+     */
+    public void theoryTest(){
 
-        int amountOfThrows = 10000;
-        int counter = 0;
-        while(counter < amountOfThrows){
+        throwDiesForRounds();
+        assertSumError();
+
+        //print array results of test
+        System.out.println();
+        System.out.print("Theoretical percentages: ");
+        printArray(theoreticalPercentage);
+        System.out.print("Calculated differences:  ");
+        printArray(sumPercentage);
+        System.out.print("Margin of error:         ");
+        printArray(difference);
+
+        //print human readable results of test
+        double max_diff = difference.stream().mapToDouble(a->a).max().getAsDouble();
+        double avg_diff = difference.stream().mapToDouble(a->a).average().getAsDouble();
+        System.out.printf("\nHighest error was: %.4f", max_diff);
+        System.out.printf("\nAverage error was: %.4f", avg_diff);
+    }
+
+    /**
+     * Throws the dice for all rounds and counts each sum into the ArrayList 'sumAmount'
+     *  and prints out the sums
+     */
+    private void throwDiesForRounds(){
+        //Throws the dice for all rounds and counts each sum into the ArrayList 'sumAmount'
+        for(int i = 0; i < rounds; i++){
             DieManager.throwDies();
-            int sumIndex = DieManager.getSum() - 2;
+            int sumIndex = DieManager.getSum() - DIE_AMOUNT;
+            //basically sumAmount += sumIndex
             sumAmount.set(sumIndex, sumAmount.get(sumIndex) + 1);
-            counter++;
         }
+        //prints out the sums
         for(int i = 0; i < sumAmount.size(); i++) {
-            System.out.println("Sum amount for " + (i + 2) + " = " + sumAmount.get(i));
+            System.out.println("Sum amount for " + (i + DIE_AMOUNT) + " = " + sumAmount.get(i));
         }
-        /* Used to copy the output for Excel document
-        System.out.println("For copying:");
-        for(int i = 0; i < sumAmount.size(); i++) {
-            System.out.println(sumAmount.get(i));
-        }
-         */
+    }
 
-        counter = 2;
-        double combinationTotal = 36;
-        ArrayList<Double> theoryPercentage = new ArrayList<>();
-        //Fills theoryPercentage with the theoretical posibility of throwing two dice
+    /**
+     * Prints the array nicely <3
+     * @param array the array to be printed
+     */
+    private void printArray(ArrayList<Double> array) {
+        System.out.print("| ");
+        for (Double _double : array) {
+            System.out.printf("%.4f", _double);
+            System.out.print(" | ");
+        }
+        System.out.println();
+    }
+
+    /**
+     * Fills theoryPercentage with the theoretical possibility of throwing two dice
+     * 'i' is the amount of possibilites for the sum 2, 3, 4...
+     * We use (i - counter) after half the array has been counted through and then we count down
+     * Therefore after the next iteration (i++),
+     *  'counter' is subtracted and we step 2 down so this iteration is actually i--
+     */
+    public void theoreticalArrayFiller() {
+        int counter = 2;
         for(int i = 1; i <= sumAmount.size(); i++) {
-            //If i is bigger than half of the size of the sumAmount, we count down
+            //If 'i' is bigger than half of the size of the sumAmount, we count down
             if (i > Math.ceil((double)sumAmount.size()/2)){
-                theoryPercentage.add(((i - counter)/ combinationTotal) * 100);
+                theoreticalPercentage.add(((i - counter)/ COMBINATION_TOTAL));
                 counter += 2;
-            } else {
-                theoryPercentage.add(i / combinationTotal * 100);
+            } else { //Else we count up
+                theoreticalPercentage.add(i / COMBINATION_TOTAL);
             }
         }
-        System.out.println("" + theoryPercentage);
+    }
 
-
-        //Divides the sum amount with amountOfThrows
-        ArrayList<Double> sumPercentage = new ArrayList<>();
-        for(int i = 0; i < sumAmount.size(); i++){
-            sumPercentage.add((sumAmount.get(i)/(double)amountOfThrows) * 100);
-        }
-        System.out.println(sumPercentage);
+    /**
+     * Fills array for sum percentage by dividing each sumAmount with the total amount of rounds
+     * Fills difference array by subtracting the theoretical theory with our sum percentage
+     * Asserts that the difference is within the accepted error
+     */
+    public void assertSumError() {
+        theoreticalArrayFiller();
 
         for (int i = 0; i < sumAmount.size(); i++) {
-            double difference = Math.abs(theoryPercentage.get(i) - sumPercentage.get(i));
-            System.out.printf("\nDifference for sum " + (i + 2) +  ": " + "%.2f", difference);
-        }
+            //Divides the sum amount with the amount of throws and adds the result to sumPercentage
+            double sum_percentage = (sumAmount.get(i) / (double) rounds);
+            sumPercentage.add(sum_percentage);
 
-        //TODO do more specific test Denice ???????????????????? -_-
+            //We use Math.abs to get positive values
+            double dif = Math.abs(theoreticalPercentage.get(i) - sumPercentage.get(i));
+            difference.add(dif);
+
+            //asserts if the difference between sumPercentage and theoryPercentage is within the acceptedError
+            assertTrue("Difference too big for sum: " + (i + 2) +
+                    " difference is " + difference.get(i), difference.get(i) < acceptedError);
+            System.out.println("sum " + (i + 2) + " passed the test");
+        }
     }
 }
