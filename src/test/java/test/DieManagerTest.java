2728dc718b06b793a9f4469700c071295643096a
SebastianHalfdanLauridsen <s215769@dtu.dk>
2021-10-26 12:10:30 +0200

    DieManagerTest expanded and working again after previous previous commit

--- src/test/java/test/DieManagerTest.java
+++ src/test/java/test/DieManagerTest.java
@@ -11,24 +11,83 @@ import static org.junit.Assert.*;
 
 public class DieManagerTest {
 
+    /**
+     * Tests if the throwDie method satisfies
+     *   the parameters of the minimum and maximum die value allowed
+     */
     @Test
     public void throwDie() {
-        Die die1 = new Die(1, 1);
-        Die die2 = new Die(1,2);
+        int die_amount = 2;
+        //create dice
+        //and adds them to DieManager's ArrayList
+        for (int i = 1; i <= die_amount; i++){
+            Die die = new Die(1, i);
+            DieManager.addDie(die);
+        }
 
-
-        DieManager.addDie(die1);
-        DieManager.addDie(die2);
+        //roll the dice
 
         DieManager.throwDies();
 
-        assertTrue("Die is out of range, die: " + die1.getFaceValue(),
-                Game.DIE_MIN_VALUE <= die1.getFaceValue() && die1.getFaceValue() <= Game.DIE_MAX_VALUE);
+        //determine if the face value of the first die is between the minimum allowed and the maximum allowed
+        for (int i = 1; i <= die_amount; i++){
+            int faceValue = DieManager.getDie(i).getFaceValue();
+            assertTrue("Die is out of range, die: " + faceValue,
+                    Game.DIE_MIN_VALUE <= faceValue && faceValue <= Game.DIE_MAX_VALUE);
+        }
 
-        assertTrue("Die is out of range, die: " + die2.getFaceValue(),
-                Game.DIE_MIN_VALUE <= die2.getFaceValue() && die2.getFaceValue() <= Game.DIE_MAX_VALUE);
+        //sum test------------------------------------------------------------------
+        ArrayList<Integer> sumAmount =  new ArrayList<>();
+        for(int i = 0; i < 11; i++){
+            sumAmount.add(i, 0);
+        }
 
-        //TODO make test
-        // - Test that the dies are theoretically accurate
+        int amountOfThrows = 10000;
+        int counter = 0;
+        while(counter < amountOfThrows){
+            DieManager.throwDies();
+            int sumIndex = DieManager.getSum() - 2;
+            sumAmount.set(sumIndex, sumAmount.get(sumIndex) + 1);
+            counter++;
+        }
+        for(int i = 0; i < sumAmount.size(); i++) {
+            System.out.println("Sum amount for " + (i + 2) + " = " + sumAmount.get(i));
+        }
+        /* Used to copy the output for Excel document
+        System.out.println("For copying:");
+        for(int i = 0; i < sumAmount.size(); i++) {
+            System.out.println(sumAmount.get(i));
+        }
+         */
+
+        counter = 2;
+        double combinationTotal = 36;
+        ArrayList<Double> theoryPercentage = new ArrayList<>();
+        //Fills theoryPercentage with the theoretical posibility of throwing two dice
+        for(int i = 1; i <= sumAmount.size(); i++) {
+            //If i is bigger than half of the size of the sumAmount, we count down
+            if (i > Math.ceil((double)sumAmount.size()/2)){
+                theoryPercentage.add(((i - counter)/ combinationTotal) * 100);
+                counter += 2;
+            } else {
+                theoryPercentage.add(i / combinationTotal * 100);
+            }
+        }
+        System.out.println("" + theoryPercentage);
+
+
+        //Divides the sum amount with amountOfThrows
+        ArrayList<Double> sumPercentage = new ArrayList<>();
+        for(int i = 0; i < sumAmount.size(); i++){
+            sumPercentage.add((sumAmount.get(i)/(double)amountOfThrows) * 100);
+        }
+        System.out.println(sumPercentage);
+
+        for (int i = 0; i < sumAmount.size(); i++) {
+            double difference = Math.abs(theoryPercentage.get(i) - sumPercentage.get(i));
+            System.out.printf("\nDifference for sum " + (i + 2) +  ": " + "%.2f", difference);
+        }
+
+        //TODO do more specific test Denice ???????????????????? -_-
     }
 }
